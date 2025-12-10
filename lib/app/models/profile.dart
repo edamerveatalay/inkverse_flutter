88e3a691fde7f6c79052e10ccfec9124828fbf88
edamerveatalay <atalayedaatalay@gmail.com>
2025-12-10 21:32:21 +0300

    feat(profile): name field has been added to the Profile model

--- lib/app/models/profile.dart
+++ lib/app/models/profile.dart
@@ -1,12 +1,14 @@
 class Profile {
   final int id;
   final int userId;
+  final String? name;
   final String? bio;
   final String? profilePhoto;
 
   Profile({
     required this.id,
     required this.userId,
+    this.name,
     this.bio,
     this.profilePhoto,
   });
@@ -16,8 +18,9 @@ class Profile {
     return Profile(
       id: json['id'],
       userId: json['user_id'],
+      name: json['name'],
       bio: json['bio'],
-      profilePhoto: json['profile_photo'],
+      profilePhoto: json['profile_image'],
     );
   }
 }
